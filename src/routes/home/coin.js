c08a5670c56923c518ec437195429fb48b15b545
Angus Russell <angus.russell89@gmail.com>
2018-01-11 22:39:31 +1000

    Added price change stats

--- src/routes/home/coin.js
+++ src/routes/home/coin.js
@@ -23,6 +23,15 @@ export default class Coin extends Component {
 		};
 	}
 
+	formatPercent = (val) => {
+		val = parseFloat(val).toFixed(1);
+		let colour = 'green'
+		if (val < 0) {
+			colour = 'red';
+		}
+		return <span style={{color: colour}}>{val}%</span>;
+	}
+
 	renderPosts = () => {
 		return this.props.coin.posts.map(post => (
 			<List.LinkItem href={post.url} target="_blank">{post.title}</List.LinkItem>
@@ -41,11 +50,14 @@ export default class Coin extends Component {
 				<Card.Primary>
 					<Card.Title large>{coin.name} (<a href={`https://coinmarketcap.com/currencies/${coin.id}`} target="_blank">{coin.symbol}</a>)</Card.Title>
 					<Card.Subtitle>
-						<div style={{display: 'inline-block', marginRight: '15px' }}><strong>Market Cap:</strong> {formatter.format(coin.mCap)}</div>
+						<div style={{display: 'inline-block', marginRight: '15px' }}><strong>Market Cap:</strong> {formatter.format(coin.market_cap_usd)}</div>
 						<div style={{display: 'inline-block', marginRight: '15px' }}><strong>CMC Rank:</strong> {coin.rank}</div>
 						<div style={{display: 'inline-block', marginRight: '15px' }}><strong>Post Mentions:</strong> {coin.postMentions}</div>
 						<div style={{display: 'inline-block', marginRight: '15px' }}><strong>Comment Mentions:</strong> {coin.commentMentions}</div>
 						<div style={{display: 'inline-block', marginRight: '15px' }}><strong>Weighted Score:</strong> {coin.totalScore.toFixed(2)}</div>
+						<div style={{display: 'inline-block', marginRight: '15px' }}><strong>1hr:</strong> {this.formatPercent(coin.percent_change_1h)}</div>
+						<div style={{display: 'inline-block', marginRight: '15px' }}><strong>24hr:</strong> {this.formatPercent(coin.percent_change_24h)}</div>
+						<div style={{display: 'inline-block', marginRight: '15px' }}><strong>7d:</strong> {this.formatPercent(coin.percent_change_7d)}</div>
 					</Card.Subtitle>
 				</Card.Primary>
 				<Card.Media className="card-media">
